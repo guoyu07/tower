74a41d0244fe3b673918cd437e6c32565c27fe61
zhu.gongping <zhu.gongping@kjt.com>
2015-06-18 09:30:40 +0800

    修复框架bug

--- tower-service/src/main/java/com/tower/service/filter/ServiceFilter.java
+++ tower-service/src/main/java/com/tower/service/filter/ServiceFilter.java
@@ -2,6 +2,7 @@ package com.tower.service.filter;
 
 import com.alibaba.dubbo.common.Constants;
 import com.alibaba.dubbo.common.extension.Activate;
+import com.alibaba.dubbo.container.spring.SpringContainer;
 import com.alibaba.dubbo.rpc.Filter;
 import com.alibaba.dubbo.rpc.Invocation;
 import com.alibaba.dubbo.rpc.Invoker;
@@ -10,33 +11,38 @@ import com.alibaba.dubbo.rpc.RpcContext;
 import com.alibaba.dubbo.rpc.RpcException;
 import com.tower.service.log.Logger;
 import com.tower.service.log.LoggerFactory;
+import com.tower.service.monitor.IMonitorService;
 import com.tower.service.util.RequestID;
 
-@Activate(group = {Constants.PROVIDER,Constants.CONSUMER})
+@Activate(group = { Constants.PROVIDER, Constants.CONSUMER })
 public class ServiceFilter implements Filter {
-    private Logger logger = LoggerFactory.getLogger(getClass());
-    private String reqidKey = "ReqId";
-    
-    public ServiceFilter(){
-        logger.info("ServiceFilter created");
-    }
-    @Override
-    public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
-        RpcContext context = RpcContext.getContext();
-        String remoteIp = context.getRemoteHost();
-        boolean provider = context.isProviderSide();
-        if(provider){
-            String reqId = context.getAttachment(reqidKey);
-            RequestID.set(reqId);
-            logger.info(reqidKey+" from client@"+remoteIp);
-        }
-        else{
-            String reqId = RequestID.get();
-            context.setAttachment(reqidKey, reqId);
-            logger.info(reqidKey+" to service@"+remoteIp);
-        }
-        Result result = invoker.invoke(invocation);
-        return result;
-    }
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	private String reqidKey = "ReqId";
+
+	public ServiceFilter() {
+		logger.info("ServiceFilter created");
+	}
+
+	@Override
+	public Result invoke(Invoker<?> invoker, Invocation invocation)
+			throws RpcException {
+
+		RpcContext context = RpcContext.getContext();
+		String remoteIp = context.getRemoteHost();
+		boolean provider = context.isProviderSide();
+		if (provider) {
+			String reqId = context.getAttachment(reqidKey);
+			RequestID.set(reqId);
+			logger.info(reqidKey + " from client@" + remoteIp);
+		} else {
+			String reqId = RequestID.get();
+			context.setAttachment(reqidKey, reqId);
+			logger.info(reqidKey + " to service@" + remoteIp);
+		}
+		Result result = invoker.invoke(invocation);
+
+		return result;
+	}
 
 }
