286f971a03406e706dfc6c3b96b1990565c0c92d
zhu.gongping <zhu.gongping@kjt.com>
2015-12-25 15:44:44 +0800

    调整脚本

--- tower-tools/publish/startJob_org.sh
+++ tower-tools/publish/startJob_org.sh
@@ -15,10 +15,12 @@ pid=`ps x | grep '$prefix-1.0-SNAPSHOT.jar' | grep -v grep | awk '{print $1}'`
 
 echo "pid:"$pid
 
- [ -n "$pid" ] &&  kill -9 $pid
+ [ -n "$pid" ] &&  kill $pid
+
+sleep 3
+
+nohup java -Ddubbo.shutdown.hook=true -Dapp.home.dir=$app_home_dir -jar $prefix-1.0-SNAPSHOT/lib/$prefix-1.0-SNAPSHOT.jar > /dev/null   &
 
 new_pid=`ps x | grep $prefix-1.0-SNAPSHOT.jar | grep -v grep | awk '{print $1}'`
 
 echo "新的进程服务pid:"$new_pid
-
-nohup java -Ddubbo.shutdown.hook=true -Dapp.home.dir=$app_home_dir -jar $prefix-1.0-SNAPSHOT/lib/$prefix-1.0-SNAPSHOT.jar > /dev/null   &
--- tower-tools/publish/startService_org.sh
+++ tower-tools/publish/startService_org.sh
@@ -4,13 +4,14 @@ prefix="$1"
 
 app_home_dir="$(dirname $(pwd))"
 
-$prefix_service_pid=`ps x | grep $prefix-service-impl-1.0-SNAPSHOT.jar | grep -v grep | awk '{print $1}'`
+tar -zxvf ../$prefix-service-impl/$prefix-service-impl-1.0-SNAPSHOT-bin.tar.gz
 
+$prefix_service_pid=`ps x | grep $prefix-service-impl-1.0-SNAPSHOT.jar | grep -v grep | awk '{print $1}'`
 
 echo "prefix_service_pid:"$$prefix_service_pid
 
- [ -n "$"$prefix"_service_pid" ] && kill -9 $$prefix_service_pid
+ [ -n "$"$prefix"_service_pid" ] && kill $$prefix_service_pid
 
-tar -zxvf ../$prefix-service-impl/$prefix-service-impl-1.0-SNAPSHOT-bin.tar.gz
+sleep 3
 
 nohup java -Ddubbo.shutdown.hook=true -Dapp.home.dir=$app_home_dir -jar $prefix-service-impl-1.0-SNAPSHOT/lib/$prefix-service-impl-1.0-SNAPSHOT.jar >/dev/null  &
