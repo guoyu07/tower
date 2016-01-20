61a5b2148f727e8939eb4e623cf5d027bd9e41ff
alex.zhu <alex.zhu@shforeigntrade.com>
2016-01-20 16:19:42 +0800

    新增获取用户全名接口

--- tower-service/src/main/java/com/tower/service/auth/UserService.java
+++ tower-service/src/main/java/com/tower/service/auth/UserService.java
@@ -7,21 +7,26 @@ import com.tower.service.domain.auth.AuthResponse;
 import com.tower.service.domain.auth.AuthUserDto;
 
 public interface UserService {
-    public AuthResponse selectList(Map<String, Object> params);
+	public AuthResponse selectList(Map<String, Object> params);
 
-    public AuthResponse insert(Map<String, Object> params);
+	public AuthResponse insert(Map<String, Object> params);
 
-    public AuthResponse updateById(Map<String, Object> params);
+	public AuthResponse updateById(Map<String, Object> params);
 
-    public AuthResponse deleteById(BigInteger id);
+	public AuthResponse deleteById(BigInteger id);
 
-    public AuthResponse selectList(Map<String, Object> params, int offset, int pageSize);
+	public AuthResponse selectList(Map<String, Object> params, int offset,
+			int pageSize);
 
-    public boolean isUserNameExist(String userName);
+	public boolean isUserNameExist(String userName);
 
-    public AuthResponse<AuthUserDto> selectById(BigInteger id);
+	public AuthResponse<AuthUserDto> selectById(BigInteger id);
 
-    public boolean isAllowDelele(BigInteger id);
+	public boolean isAllowDelele(BigInteger id);
 
-    public AuthUserDto selectByTicket(String ticketValidateUrl, String ticket);
+	public AuthUserDto selectByTicket(String ticketValidateUrl, String ticket);
+
+	public String selectFullNameByUserName(String userName);
+
+	public String selectFullNameByUserId(BigInteger userId);
 }
