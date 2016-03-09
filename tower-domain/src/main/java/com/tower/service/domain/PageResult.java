3e7068057c21fd08ca862ab850967d7c90229144
alex.zhu <alex.zhu@shforeigntrade.com>
2016-03-09 20:42:15 +0800

    调整pageResult

--- tower-domain/src/main/java/com/tower/service/domain/PageResult.java
+++ tower-domain/src/main/java/com/tower/service/domain/PageResult.java
@@ -9,6 +9,20 @@ public class PageResult<T> implements IResult{
 	 */
 	private static final long serialVersionUID = -6656398713432429279L;
 
+	private int pageIndex;
+	private int pageSize;
+	private List<T> result;
+	
+	public PageResult(int pageIndex, int pageSize){
+		this.pageIndex = pageIndex;
+		this.pageSize = pageSize;
+	}
+	public PageResult(int pageIndex, int pageSize,List<T> result){
+		this.pageIndex = pageIndex;
+		this.pageSize = pageSize;
+		this.result= result;
+	}
+	
 	public int getPageIndex() {
 		return pageIndex;
 	}
@@ -26,36 +40,6 @@ public class PageResult<T> implements IResult{
 	}
 	public void setResult(List<T> result) {
 		this.result = result;
-	}
-	
-	private int total = 0; // 总记录数
-	public int getTotal() {
-		return total;
-	}
-	public void setTotal(int total) {
-		this.total = total;
-	}
-	public int getPageCount() {
-		return pageCount;
-	}
-	public void setPageCount(int pageCount) {
-		this.pageCount = pageCount;
-	}
-
-	private int pageCount = 0; // 总页数
-	
-	private int pageIndex;
-	private int pageSize;
-	private List<T> result;
-	
-	public PageResult(int pageIndex, int pageSize){
-		this.pageIndex = pageIndex;
-		this.pageSize = pageSize;
-	}
-	public PageResult(int pageIndex, int pageSize,List<T> result){
-		this.pageIndex = pageIndex;
-		this.pageSize = pageSize;
-		this.result= result;
 	}
 	
 }
