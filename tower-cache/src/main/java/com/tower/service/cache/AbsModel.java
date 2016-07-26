ed6bb617f7d4719da5d5a42ea4db7f7f5dfbbe23
arch_level <arch_level@126.com>
2016-07-26 13:36:26 +0800

    覆盖 Model & DTO toString 、hashCode、 ecquals 实现

--- tower-cache/src/main/java/com/tower/service/cache/AbsModel.java
+++ tower-cache/src/main/java/com/tower/service/cache/AbsModel.java
@@ -1,5 +1,7 @@
 package com.tower.service.cache;
 
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.apache.commons.lang.builder.ToStringBuilder;
 
 public abstract class AbsModel implements IModel {
@@ -9,7 +11,15 @@ public abstract class AbsModel implements IModel {
 	 */
 	private static final long serialVersionUID = 4470326630561962443L;
 
-	public String toString(){
-    	return ToStringBuilder.reflectionToString(this);
-    }
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this);
+	}
+
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
+	}
+	
+	public   boolean  equals(Object o)  {
+	     return  EqualsBuilder.reflectionEquals( this , o);
+	}
 }
--- tower-domain/src/main/java/com/tower/service/domain/AbsDTO.java
+++ tower-domain/src/main/java/com/tower/service/domain/AbsDTO.java
@@ -1,5 +1,7 @@
 package com.tower.service.domain;
 
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.apache.commons.lang.builder.ToStringBuilder;
 
 
@@ -14,7 +16,15 @@ public abstract class AbsDTO implements IDTO{
 	 * 
 	 */
 
-	public String toString(){
-    	return ToStringBuilder.reflectionToString(this);
-    }
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this);
+	}
+
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
+	}
+	
+	public   boolean  equals(Object o)  {
+	     return  EqualsBuilder.reflectionEquals( this , o);
+	}
 }
